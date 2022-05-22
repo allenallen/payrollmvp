02f0cc8bdcf517df7fe0032b603f604104e7a8fb
allenallen <allenarcenal@gmail.com>
2022-05-22 17:48:10 +0800

    removed UserDAO

--- src/main/java/com/tamaraw/payroll/controllers/LoginController.java
+++ src/main/java/com/tamaraw/payroll/controllers/LoginController.java
@@ -1,16 +1,12 @@
 package com.tamaraw.payroll.controllers;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
-import com.tamaraw.payroll.HelloApplication;
-import com.tamaraw.payroll.daos.UserDAO;
 import com.tamaraw.payroll.models.User;
 import com.tamaraw.payroll.services.UserService;
 import com.tamaraw.payroll.utils.APIDefinitions;
 import com.tamaraw.payroll.utils.SceneLoader;
 import com.tamaraw.payroll.utils.Scenes;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
--- src/main/java/com/tamaraw/payroll/models/User.java
+++ src/main/java/com/tamaraw/payroll/models/User.java
@@ -1,21 +1,12 @@
 package com.tamaraw.payroll.models;
 
-import com.tamaraw.payroll.daos.UserDAO;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class User extends BaseDBObject<User>{
+public class User {
 
     private StringProperty userName = new SimpleStringProperty();
     private StringProperty password = new SimpleStringProperty();
-
-    public User(ResultSet rs) throws SQLException {
-        this.setUserName(rs.getString(UserDAO.USERNAME_COLUMN));
-        this.setPassword(rs.getString(UserDAO.PASSWORD_COLUMN));
-    }
 
     public User(UserDto dto) {
         this.setUserName(dto.getUsername());
@@ -38,8 +29,4 @@ public class User extends BaseDBObject<User>{
         this.password.set(password);
     }
 
-    @Override
-    User initFromResultSet(ResultSet resultSet) throws SQLException {
-        return new User(resultSet);
-    }
 }
